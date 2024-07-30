a6faa472b26869696fe1df408cfa7505a4c88057
unknown <amaan2228web@gmail.com>
2024-07-30 18:04:47 +0530

    Notes-Project

---
+++ Notes-Project/src/Components/Notes.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useRef } from 'react'
+
+export const Notes = () => {
+
+    const [notes, setNotes] = useState([]);
+    const noteRefs = useRef([]);
+
+    const createNote = () => {
+        const newNote = { id: Date.now(), content: '' };
+        setNotes([...notes, newNote]);
+        noteRefs.current.push(React.createRef());
+    };
+
+    const updateNote = (index) => {
+        const newNotes = [...notes];
+        newNotes[index].content = noteRefs.current[index].current.textContent;
+        setNotes(newNotes);
+    };
+
+    return (
+        <div>
+            <div>
+                <h1>Notes Project</h1>
+                <button onClick={createNote}>Create Note</button>
+            </div>
+            
+            <div>
+                {notes.map((note, index) => (
+                    <div key={note.id} className='note-box'>
+                        <p 
+                            contentEditable='true'
+                            ref={noteRefs.current[index]}
+                            onInput={() => updateNote(index)}
+                        >
+                        </p>
+                    </div>
+                ))}
+            </div>
+        </div>
+  )
+}
